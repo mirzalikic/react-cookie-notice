12ebd22639e03bcf546f9f85014cdfd8e58501e5
Mirza Likic <mirza.likic@gmail.com>
2020-09-27 21:02:48 +0200

    new build

---
+++ precache-manifest.83989780ed142e96d14af7c5d77b0b8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c630f2e7ecfe6fb891c79d749660c4",
+    "url": "/react-cookie-notice/index.html"
+  },
+  {
+    "revision": "2e68af2fe60d9295e8e3",
+    "url": "/react-cookie-notice/static/css/main.cafc69d7.chunk.css"
+  },
+  {
+    "revision": "7b272ea8dcb9e203fd4d",
+    "url": "/react-cookie-notice/static/js/2.c2c5ab18.chunk.js"
+  },
+  {
+    "revision": "f4afed48d85afa3f34462114eda5d16c",
+    "url": "/react-cookie-notice/static/js/2.c2c5ab18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e68af2fe60d9295e8e3",
+    "url": "/react-cookie-notice/static/js/main.3035a36d.chunk.js"
+  },
+  {
+    "revision": "7b03998bd439ac2644f0",
+    "url": "/react-cookie-notice/static/js/runtime-main.2069790d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-cookie-notice/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f605c914b97f320107e8c1b6e31c61ae.js"
+  "/react-cookie-notice/precache-manifest.83989780ed142e96d14af7c5d77b0b8e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-cookie-notice/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
